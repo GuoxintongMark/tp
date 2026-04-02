a66466de5c15880a2d4fb665e06a07f793ffb46a
EepyAdventure <joshua.poon181818@gmail.com>
2026-04-02 23:20:44 +0800

    Fix method in SampleDataUtil

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -53,19 +53,15 @@ public class SampleDataUtil {
         Delivery[] deliveries = new Delivery[] {
             new Delivery(new Product("iPhone"), apple,
                 new Deadline(date + " 14:30"),
-                new seedu.address.model.delivery.Address("78 Airport Blvd, #02-234"),
                 getTagSet("fragile")),
             new Delivery(new Product("laptop"), dell,
                 new Deadline(date + " 14:30"),
-                new seedu.address.model.delivery.Address("Changi Business Park Central 1"),
                 getTagSet("test")),
             new Delivery(new Product("tablet"), samsung,
                 new Deadline(date + " 14:30"),
-                new seedu.address.model.delivery.Address("313 Orchard Rd"),
                 getTagSet("fragile")),
             new Delivery(new Product("printer"), hp,
                 new Deadline(date + " 14:30"),
-                new seedu.address.model.delivery.Address("750 Chai Chee Road, #01-01"),
                 getTagSet("heavy")),
         };
 
